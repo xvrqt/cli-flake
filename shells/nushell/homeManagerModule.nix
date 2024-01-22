a025567830d64881d495adfd5b3cb59c22838e65
xvrqt <git@xvrqt.com>
2024-01-21 16:16:52 -0800

    Fix Nu shell aliases idiosyncracies

--- shells/nushell/homeManagerModule.nix
+++ shells/nushell/homeManagerModule.nix
@@ -20,27 +20,14 @@ in {
         shellAliases = {
           ## Convenience
           c = "clear";
+
           # Common Typos
           ".." = "cd ..";
           "cd.." = "cd ..";
+
           # Resume wget downloads
           wget = "wget -c";
           top = "atop";
-
-          ## INFO
-          h = "history";
-          j = "jobs -l";
-          now = "date +\"%T\"";
-          ping = "ping -c 5";
-          ports = "netstat -tulnap";
-          meminfo = "free -m -l -t";
-          # Top process eating memory
-          psmem = "ps auxf | sort -nr -k 4";
-          psmem10 = "ps auxf | sort -nr -k 4 | head -10";
-          cpuinfo = "lscpu";
-          # Top process eating CPU
-          pscpu = "ps auxf | sort -nr -k 3";
-          pscpu10 = "ps auxf | sort -nr -k 3 | head -10";
 
           ## SAFETY
           rm = "rm -I --preserve-root";
