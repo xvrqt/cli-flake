7c504dd04efaedeea71c9ed1ed8411ca977df8e0
xvrqt <git@xvrqt.com>
2024-02-01 14:07:30 -0800

    pain

--- coreUtils/bat/homeManagerModule.nix
+++ coreUtils/bat/homeManagerModule.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }: let
@@ -8,13 +9,20 @@ in {
   config = lib.mkIf config.cli.coreUtils.enable {
     programs.bat = {
       enable = setDef true;
+      themes = {
+        catppuccin-mocha = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            sha256 = null;
+          };
+          file = "catppuccin-mocha.tmTheme";
+        };
+      };
       config = {
         theme = setDef "catppuccin-mocha";
       };
-    };
-    # Copy the custom themes to the `bat` config directory
-    home.file = {
-      ".config/bat/themes/catppuccin-mocha.tmTheme".source = ./themes/catppuccin-mocha.tmTheme;
     };
   };
 }
