bf17bd91ece17862b92eff098abb0fdf56702679
xvrqt <git@xvrqt.com>
2025-01-24 14:27:53 -0800

    Added langserv support to helix

--- productivity/helix/helix.nix
+++ productivity/helix/helix.nix
@@ -1,45 +1,182 @@
-{
-  lib,
-  pkgs,
-  config,
-  ...
-}: let
-  setDef = lib.mkOverride 1000;
-in {
-  config = lib.mkIf config.cli.productivity.enable {
-    programs.helix = {
-      enable = setDef true;
-      extraPackages = with pkgs; [
-        bash-language-server
-        biome
-        clang-tools
-        docker-compose-language-service
-        dockerfile-language-server-nodejs
-        golangci-lint
-        golangci-lint-langserver
-        gopls
-        gotools
-        helix-gpt
-        marksman
-        nil
-        nixd
-        nixpkgs-fmt
-        nodePackages.prettier
-        nodePackages.typescript-language-server
-        sql-formatter
-        ruff
-        (python3.withPackages (p: (with p; [
-          python-lsp-ruff
-          python-lsp-server
-        ])))
-        rust-analyzer
-        taplo
-        taplo-lsp
-        terraform-ls
-        typescript
-        vscode-langservers-extracted
-        yaml-language-server
-      ];
+{ config, pkgs, ... }: {
+  home-manager.users.${config.user}.programs.helix = with pkgs; {
+    enable = true;
+    defaultEditor = true;
+    extraPackages = [
+      bash-language-server
+      biome
+      clang-tools
+      docker-compose-language-service
+      dockerfile-language-server-nodejs
+      golangci-lint
+      golangci-lint-langserver
+      gopls
+      gotools
+      helix-gpt
+      marksman
+      nil
+      nixd
+      nixpkgs-fmt
+      nodePackages.prettier
+      nodePackages.typescript-language-server
+      sql-formatter
+      ruff
+      (python3.withPackages (p: (with p; [
+        python-lsp-ruff
+        python-lsp-server
+      ])))
+      rust-analyzer
+      taplo
+      taplo-lsp
+      terraform-ls
+      typescript
+      vscode-langservers-extracted
+      yaml-language-server
+    ];
+
+    settings = {
+      theme = "gruvbox_community";
+
+      editor = {
+        color-modes = true;
+        cursorline = true;
+        bufferline = "multiple";
+
+        soft-wrap.enable = true;
+
+        auto-save = {
+          focus-lost = true;
+          after-delay.enable = true;
+        };
+
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        file-picker = {
+          hidden = false;
+          ignore = false;
+        };
+
+        indent-guides = {
+          character = "┊";
+          render = true;
+          skip-levels = 1;
+        };
+
+        end-of-line-diagnostics = "hint";
+        inline-diagnostics.cursor-line = "warning";
+
+        lsp = {
+          display-inlay-hints = true;
+          display-messages = true;
+        };
+
+        statusline = {
+          left = [ "mode" "file-name" "spinner" "read-only-indicator" "file-modification-indicator" ];
+          right = [ "diagnostics" "selections" "register" "file-type" "file-line-ending" "position" ];
+          mode.normal = "";
+          mode.insert = "I";
+          mode.select = "S";
+        };
+      };
+
+      keys = {
+        normal = {
+          H = "extend_char_left";
+          x = "extend_to_line_bounds";
+          J = [ "extend_line_down" "extend_to_line_bounds" ];
+          K = [ "extend_line_up" "extend_to_line_bounds" ];
+          L = "extend_char_right";
+          B = "extend_prev_word_start";
+          W = "extend_next_word_start";
+          E = "extend_next_word_end";
+          A-b = "move_prev_long_word_start";
+          A-B = "extend_prev_long_word_start";
+          A-w = "move_next_long_word_start";
+          A-W = "extend_next_long_word_start";
+          A-e = "move_next_long_word_end";
+          A-E = "extend_next_long_word_end";
+          T = "extend_till_char";
+          F = "extend_next_char";
+          A-t = "till_prev_char";
+          A-f = "find_prev_char";
+          A-T = "extend_till_prev_char";
+          A-F = "extend_prev_char";
+          A-j = "join_selections";
+          A-k = "keep_selections";
+          M = [ "select_mode" "match_brackets" "normal_mode" ];
+          "#" = "toggle_comments";
+          A-l = "extend_to_line_end";
+          A-h = "extend_to_line_start";
+          left = "goto_previous_buffer";
+          right = "goto_next_buffer";
+          A-d = "delete_selection";
+          c = "change_selection_noyank";
+          d = "delete_selection_noyank";
+          N = "extend_search_next";
+          A-n = "search_prev";
+          A-N = "extend_search_prev";
+          C-d = [ "page_cursor_half_down" "align_view_center" ];
+          C-u = [ "page_cursor_half_up" "align_view_center" ];
+          tab = "move_parent_node_end";
+          S-tab = "move_parent_node_start";
+
+          G = {
+            j = "@vgj<esc>";
+            k = "@vgk<esc>";
+          };
+
+          g = {
+            j = "goto_last_line";
+            k = "goto_file_start";
+          };
+
+          space = {
+            c = ":buffer-close";
+            A-f = ":toggle auto-format";
+            q = ":write-quit-all";
+            Q = ":quit!";
+            e = ":config-open";
+            w = ":write";
+            "." = ":toggle file-picker.git-ignore";
+          };
+        };
+
+        insert = {
+          C-u = [ "extend_to_line_bounds" "delete_selection_noyank" "open_above" ];
+          C-w = [ "move_prev_word_start" "delete_selection_noyank" ];
+          C-space = "completion";
+          S-tab = "move_parent_node_start";
+        };
+
+        select = {
+          tab = "extend_parent_node_end";
+          S-tab = "extend_parent_node_start";
+
+          g = {
+            j = "goto_last_line";
+            k = "goto_file_start";
+          };
+        };
+      };
+    };
+
+    themes = {
+      # https://github.com/helix-editor/helix/blob/master/runtime/themes/gruvbox.toml
+      gruvbox_community = {
+        inherits = "gruvbox";
+        "variable" = "blue1";
+        "variable.parameter" = "blue1";
+        "function.macro" = "red1";
+        "operator" = "orange1";
+        "comment" = "gray";
+        "constant.builtin" = "orange1";
+        "ui.background" = { };
+      };
+    };
 
     languages = {
       language-server.biome = {
@@ -231,65 +368,6 @@ in {
           auto-format = true;
         }
       ];
-    };
-  };
-
-      settings = {
-        theme = "catppuccin_mocha";
-        editor = {
-          color-modes = true;
-          cursorline = true;
-          bufferline = "multiple";
-
-          soft-wrap.enable = true;
-
-          auto-save = {
-            focus-lost = true;
-            after-delay.enable = true;
-          };
-
-          cursor-shape = {
-            insert = "bar";
-            normal = "block";
-            select = "underline";
-          };
-
-          file-picker = {
-            hidden = false;
-            ignore = false;
-          };
-
-          indent-guides = {
-            character = "┊";
-            render = true;
-            skip-levels = 1;
-          };
-
-          end-of-line-diagnostics = "hint";
-          inline-diagnostics.cursor-line = "warning";
-
-          lsp = {
-            display-inlay-hints = true;
-            display-messages = true;
-          };
-
-          statusline = {
-            left = ["mode" "file-name" "spinner" "read-only-indicator" "file-modification-indicator"];
-            right = ["diagnostics" "selections" "register" "file-type" "file-line-ending" "position"];
-            mode.normal = "";
-            mode.insert = "I";
-            mode.select = "S";
-          };
-        };
-      };
-    };
-
-    home = {
-      #packages = [pkgs.ueberzugpp pkgs.unar];
-      packages = [pkgs.unar]; # Removed Ueberzugpp because it wasn't building and doesn't work anyways
-      file = {
-        ".config/helix/themes/default.toml".source = ./themes/catppuccin-mocha.toml;
-      };
     };
   };
 }
